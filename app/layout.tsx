f7f96f91e677d5e5bfd9f0e707ec0dd2dc6d5660
shayanel <shayanelhoque@gmail.com>
2026-09-19 19:52:52 -0500

    Wrap Tapmo with Privy provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import TapmoPrivyProvider from "@/components/TapmoPrivyProvider";
 
 export const metadata: Metadata = {
   title: "Tapmo",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <TapmoPrivyProvider>{children}</TapmoPrivyProvider>
+      </body>
     </html>
   );
 }
